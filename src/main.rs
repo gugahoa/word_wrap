74bdb3846bb1085410264b11d2c1ee2e9dc68af8
Gustavo Aguiar <gugahoa@gmail.com>
2016-12-04 21:58:59 -0200

    Iterative solution

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,28 @@
-fn word_wrap_solution(i: i32, m: i32, words_length: &Vec<i32>, memoization_vec: &mut Vec<i32>) -> Option<i32> {
-    if i < 0 {
-        return None;
-    }
+fn word_wrap_solution(n: i32, line_size: i32, words_length: &Vec<i32>) -> i32 {
+    let mut memoization_vec = vec![i16::max_value() as i32; (n + 2) as usize];
+    memoization_vec[0] = 0;
+    memoization_vec[1] = (line_size - words_length[0]).pow(2);
+    for i in 1..n+2 {
+        let mut line_cost = line_size + 1;
+        let start = std::cmp::max(1, i - (line_size/2) as i32 +1);
+        for j in (start..i+1).rev() {
+            line_cost -= words_length[(j - 1) as usize] + 1;
+            if line_cost < 0 {
+                break;
+            }
 
-    if memoization_vec[i as usize] >= 0 {
-        return Some(memoization_vec[i as usize]);
-    }
-
-    let result = (0..i+1).into_iter().map(|x| {
-        let base_cost = m - words_length[x as usize];
-        let remaining_cost = (x+1..i+1).into_iter().map(|j| 1 + words_length[j as usize]).sum::<i32>();
-        let cost = base_cost - remaining_cost;
-        let quadratic_cost;
-        if cost < 0 {
-            quadratic_cost = i16::max_value() as i32;
-        } else {
-            quadratic_cost = cost * cost;
+            let real_cost = line_cost.pow(2);
+            if memoization_vec[(j - 1) as usize] + real_cost < memoization_vec[i as usize] {
+                memoization_vec[i as usize] = memoization_vec[(j - 1) as usize] + real_cost;
+            }
         }
-
-        match word_wrap_solution(x - 1, m, words_length, memoization_vec) {
-            Some(result) => result + quadratic_cost,
-            None => quadratic_cost
-        }
-    }).min();
-
-    if let Some(save) = result {
-        memoization_vec[i as usize] = save;
     }
 
-    return result;
+    println!("memo vec {:?}", memoization_vec);
+    memoization_vec[n as usize]
 }
 
 fn main() {
     let words_length = vec![3, 2, 2, 5];
-    let mut memoization_vec = vec![-1; words_length.len()];
-    println!("word_wrap_solution({}, {}, {:?}) = {:?}", 3, 6, words_length, word_wrap_solution(3, 6, &words_length, &mut memoization_vec));
-    println!("memoization_vec = {:?}", memoization_vec);
+    word_wrap_solution(3, 6, &words_length);
 }
